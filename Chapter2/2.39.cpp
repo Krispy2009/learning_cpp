92c10ac7ac79d2da7b035ad3e958a9decdf961d8
Kristi Achilleos <Krispy2009@gmail.com>
2015-10-09 17:59:59 +0100

    Intro to structs! don't forget the ;

---
+++ Chapter2/2.39.cpp
@@ -0,0 +1,10 @@
+struct Foo {/* empty */}
+/*
+* compiler will complain:
+* error: expected ‘;’ after struct definition
+*/
+
+int main()
+{
+	return 0;
+}
---
+++ Chapter2/2.40.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+struct Sales_data
+{
+	std::string book_no;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+int main() {
+	return 0;
+}
