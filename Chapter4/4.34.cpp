0f59dae70ee9a5637623471e39683abbe4854c65
Kristi Achilleos <Krispy2009@gmail.com>
2015-10-23 19:20:00 +0100

    Arithmetic conversions

---
+++ Chapter4/4.34.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*(a) if(fval)
+
+	if fval is 0 then condition is false, else fval is converted to 1 and
+	condition is true.
+
+*/
+
+/*(b) dval = fval + ival
+
+	ival is converted to float and the result is converted to double.
+
+*/
+
+/*(c) dval + ival * cval
+
+	cval is converted to integer and the result is converted to double.
+
+*/
---
+++ Chapter4/4.35.cpp
@@ -0,0 +1,11 @@
+/*
+	(a)  cval will not change
+
+	(b) ival and ui will be converted to doubles and the result will be
+		converted to a float.
+
+	(c) dval will stay a double.
+
+	(d) fval and ival will be converted to doubles and the result will be
+		converted to char.
+*/
