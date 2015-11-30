7bc497fbb4ad34f4be56e2bc9d8cc4008885c328
Kristi Achilleos <Krispy2009@gmail.com>
2015-11-30 00:57:00 +0000

    Constructors revisited

---
+++ Chapter7/7.40.cpp
@@ -0,0 +1,34 @@
+/*
+a) Book - data: author, title
+        - constructors: Book(), Book(string title), Book(string author, string title)
+        
+b) Date - data: day, month, year
+        - constructors: Date(int day=1, int month=1, int year=1970),
+                        Date(int day, int month=1, int year=1970)
+                        Date(int day, int month, int year=1970)
+                        Date(int day, int month, int year)
+c) Employee - data: name, age, position
+            - constructors: Employee()
+                            Employee(string name);
+                            Employee(string name, int age);
+                            Employee(string name, int age, string position);
+                            Employee(string name, string position);
+                            Employee(int age);
+                            
+d) Vehicle - data: brand, reg_year, plates
+           - constructors: Vehicle()
+                            Vehicle(string plates)
+                            Vehicle(string plates, string brand)
+                            Vehicle(string plates, string brand, int reg_year)
+
+e) Object - data: sides
+          - constructors: Object()
+                          Object(int sides);
+
+f) Tree - data: height, type
+        - constructors: Tree();
+                        Tree(int height)
+                        Tree(int height, string type)
+                        Tree(string type)
+
+*/
